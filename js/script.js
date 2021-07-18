a76cad8971b4c8fddf9c13fd328fed31196d76ed
André Matiello C. Caramanti <matiello.andre@hotmail.com>
2021-07-18 01:05:44 -0300

    landing-finalizada

--- js/script.js
+++ js/script.js
@@ -1,32 +1,10 @@
-/*Elementos*/
- const inputPeople = document.querySelector("email")
- const btnSavePeople = document.querySelector("box-button")
- const result = document.querySelector("#result ol")
- 
- /*Função responsável por salvar os dados*/
- btnSavePeople.addEventListener("click", _ => {
-   let peoples = new Array()
- 
-   /*Verifica se a propriedade existe, caso exista, converte de String para Object*/
-   if (localStorage.hasOwnProperty("peoples")) {
-     peoples = JSON.parse(localStorage.getItem("peoples"))
-   }
- 
-   /*Adiciona um novo valor no array criado*/
-   peoples.push({name: inputPeople.value})
- 
-   /*Salva o item*/
-   localStorage.setItem("peoples", JSON.stringify(peoples))
- 
-   /*Exibe o resultado*/
-   result.insertAdjacentHTML('beforeend', `<li>${inputPeople.value}</li>`)
- })
-
- /*Função responsável por carregar o conteúdo*/
-window.addEventListener("load", _ => {
-  if (localStorage.hasOwnProperty("peoples")) {
-    JSON.parse(localStorage.getItem("peoples")).forEach(people => {
-      result.insertAdjacentHTML('beforeend', `<li>${people.name}</li>`)
-    })    
-  }
+const form = document.getElementById('form')
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    let email = document.getElementById('email').value
+    let data = {email};
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData);
+    let message = document.getElementById('message')
+    document.getElementById('email').value = ' ' 
 })
